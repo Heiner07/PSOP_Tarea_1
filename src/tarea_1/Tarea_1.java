f43ef4c345fb37949d9a82083b4134970c4a1f9d
Heiner <Heiner@DELL-AGENT007>
2019-09-02 16:06:07 -0600

    Eliminé lineas de comentarios innecesarias

--- src/tarea_1/Tarea_1.java
+++ src/tarea_1/Tarea_1.java
@@ -20,23 +20,6 @@ public class Tarea_1 {
     public static void main(String[] args) throws IOException {
         Operaciones op = new Operaciones();
         JFVentanaPrincipal ventana=new JFVentanaPrincipal();
-        ventana.setVisible(true);
-       /* String prueba1 = "0011 0001 00000101";
-        String prueba2 = "0011 0010 00000011";
-        String prueba3 = "0001 0001 00000000";
-        String prueba4 = "0101 0010 00000000";
-        String prueba5 = "0100 0001 00000000";
-        String prueba6 = "0010 0001 00000000";   
-        
-        op.Operaciones(prueba1);
-        op.Operaciones(prueba2);
-        op.Operaciones(prueba3);
-        op.Operaciones(prueba4);
-        op.Operaciones(prueba5);
-        op.Operaciones(prueba6);
-        */
-        //Operaciones.muestraContenido("C:\\Users\\Heine\\Documents\\1.TEC\\Principios de Sistemas Operativos\\instrucciones.asm");
-        
-    }
-    
+        ventana.setVisible(true);   
+    }    
 }
